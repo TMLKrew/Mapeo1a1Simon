9be0dec3fc7efc557341a4e8e8472bc55074de8d
SIMON-PORTATIL\Simon <simon_acb@hotmail.com>
2018-01-12 12:46:45 +0100

    Aplicacion MapeoUnoAUno con hibernate

---
+++ MapeoUnoAUnoSimon/src/main/java/es/albarregas/beans/Servicio.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.albarregas.beans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Simon
+ */
+public class Servicio implements Serializable{
+    private Long id;
+    private String nombre;
+    private String path;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+    
+    
+}
